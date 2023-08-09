1ae28972678fd13bae60ad83b186ae2bd17e354b
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-08-09 14:42:52 +0200

    finbal1

--- main.ts
+++ main.ts
@@ -1,40 +1,39 @@
-let time = ""
 let valeure = 0
 datalogger.setColumnTitles(
 "time",
 "Distance"
 )
+let time = ""
 let heure = 0
 let minute = 0
 let secondes = 0
 let comp = 0
-while (true) {
+basic.forever(function () {
+    secondes += 1
+    if (secondes == 60) {
+        minute += 1
+        secondes = 0
+    }
+    if (minute == 60) {
+        heure += 1
+        minute = 0
+    }
+    time = "" + convertToText(heure) + ":" + convertToText(minute) + ":" + convertToText(secondes)
     valeure = Math.map(pins.analogReadPin(AnalogPin.P0), 250, 730, 0, 25)
-    if (input.lightLevel() > 50) {
+    if (input.lightLevel() > 10) {
         led.plotBarGraph(
         valeure,
         0
         )
     }
-    basic.pause(500)
     if (comp == 10) {
         datalogger.log(
-        datalogger.createCV("Distance", pins.analogReadPin(AnalogPin.P0)),
-        datalogger.createCV("time", time)
+        datalogger.createCV("time", time),
+        datalogger.createCV("Distance", pins.analogReadPin(AnalogPin.P0))
         )
         comp = 0
     } else {
         comp += 1
     }
-}
-basic.forever(function () {
-    secondes += 1
-    if (secondes == 60) {
-        minute += 1
-    }
-    if (minute == 60) {
-        heure += 1
-    }
-    time = "" + heure + ":" + minute + ":" + secondes
     basic.pause(1000)
 })
